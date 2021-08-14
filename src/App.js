0e5c80e01e94d1e93ba1cb1e4163100f23c2dc70
Joey-Perry <Joeyap84@gmail.com>
2021-08-14 15:30:08 -0500

    set up initial front end code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
+import Nav from './components/Nav/Nav';
 import './App.css';
 
 class App extends Component{
@@ -14,6 +15,7 @@ class App extends Component{
   render(){
     return(
       <div className='App'>
+        <Nav />
         Hello World
       </div>
     )
